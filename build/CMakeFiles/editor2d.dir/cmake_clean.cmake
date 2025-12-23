45d4790019efad7ebc58dcc69df5b121cfeea5ba
Joel <jooeeloo.06@gmail.com>
2025-12-23 11:38:49 +0100

    Initial project setup with CMake

---
+++ build/CMakeFiles/editor2d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/editor2d.dir/src/main.cpp.o"
+  "editor2d"
+  "editor2d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/editor2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Editor2D starting...\n";
+    return 0;
+}
