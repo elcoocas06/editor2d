402a0b9de2cf229e892354d2a3a87e561b614f25
Jordi Coca <jooeeloo.06@gmail.com>
2025-12-24 12:09:32 +0100

    Add SDL2 window and ignore build directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
+
+#include <SDL2/SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Editor2D starting...\n";
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Editor2D",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
